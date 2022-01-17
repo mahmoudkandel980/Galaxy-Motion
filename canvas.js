fe49910e942ba20bdde2c792b43d683f770bf37f
mahmoud kandel <mahmoudkandel980@gmail.com>
2022-01-17 04:48:51 +0200

    audio2

--- canvas.js
+++ canvas.js
@@ -93,7 +93,13 @@ function animate() {
     radians += 0.0025
   } else if (!mouseDown) {
     radians += 0.001
-
+  }
+  let isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+  if (!isChrome) {
+    $('#iframeAudio').remove()
+  }
+  else {
+    $('#playAudio').remove() // just to make sure that it will not have 2x audio in the background 
   }
 }
 
